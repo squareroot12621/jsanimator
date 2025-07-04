0593ed39ed9d1411df9192860e87eb904dc6ce51
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 14:43:04 -0500

    Even more error-detecting

--- js/create_element.js
+++ js/create_element.js
@@ -1,10 +1,14 @@
 function create_element(tag, content=[], attributes={}) {
   var element = document.createElement(tag)
-  if (content) {
-    /* Passing a string should still work,
-       because it's an array of characters */
+  
+  if (typeof content === 'object') {
     element.append(...content)
+  } else if (typeof content === 'string') {
+    element.append(content)
+  } else {
+    throw new TypeError('content must be an array or a string')
   }
+  
   for (var [key, value] of Object.entries(attributes)) {
     element.setAttribute(key, value)
   }
