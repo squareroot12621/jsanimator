d4425ee5d21d3629172e1cf78889cc6f55c8e990
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 13:50:34 -0500

    Create loading.js

---
+++ js/loading.js
@@ -0,0 +1,15 @@
+function create_loading_screen() {
+  var loading_paragraph = document.createElement('p')
+  loading_paragraph.append('Loading...')
+  
+  var loading_container = document.createElement('div')
+  loading_container.setAttribute('id', 'scripterror')
+  loading_container.append(loading_paragraph)
+  
+  var js_animator = document.getElementById('jsanimator')
+  js_animator.replaceChildren(loading_container)
+}
+
+export {
+  create_loading_screen,
+}
