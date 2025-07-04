8a394de93331572e855f26595b761f58a83b9f0c
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 14:16:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import create_loading_screen from './loading.js'
+import { create_loading_screen } from './loading.js'
 
 function initialize_js_animator() {
   document.getElementById('jsanimator').replaceChildren()
