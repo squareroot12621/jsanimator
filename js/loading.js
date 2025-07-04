4280132f8c0c4e65dcb4361b7257832facd795fd
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 15:35:09 -0500

    Add #loadingtext to loading_paragraph

--- js/loading.js
+++ js/loading.js
@@ -3,15 +3,17 @@ import { create_element } from './create_element.js'
 function create_loading_screen() {
   var loading_dots = []
     for (var dot = 1; dot <= 3; ++dot) {
-      loading_dots.push(create_element(
-        'span',
-        '.',
-        {'class': 'loadingdot',
-         'id': 'loadingdot' + dot.toString()}
-      ))
+      loading_dots.push(create_element('span',
+                                       '.',
+                                       {'class': 'loadingdot',
+                                        'id': 'loadingdot' + dot.toString()}))
     }
-  var loading_paragraph = create_element('p', ['Loading', ...loading_dots])
-  var loading_container = create_element('div', loading_paragraph, {'id': 'scripterror'})
+  var loading_paragraph = create_element('p',
+                                         ['Loading', ...loading_dots],
+                                         {'id': 'loadingtext'})
+  var loading_container = create_element('div',
+                                         loading_paragraph,
+                                         {'id': 'scripterror'})
   
   var js_animator = document.getElementById('jsanimator')
   js_animator.replaceChildren(loading_container)
