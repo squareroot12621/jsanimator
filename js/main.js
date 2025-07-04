9c1f0f8e8ab940560beaad23ff72d6a1602c9d5a
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 18:16:00 -0500

    Is JSDelivr supreme once again?

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { create_loading_screen, load_module } from './loading.js'
 
 function initialize_js_animator() {
   create_loading_screen()
-  load_module('Mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.js?a4098')
+  load_module('Mousetrap', 'https://cdn.jsdelivr.net/gh/ccampbell/mousetrap/mousetrap.min.js')
   // TODO: setInterval?
 }
 
