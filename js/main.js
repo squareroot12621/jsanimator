6717695aa704c1a6f1bae48587d2cc65b213a123
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 18:04:26 -0500

    Oops, CORS got in the way

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { create_loading_screen, load_module } from './loading.js'
 
 function initialize_js_animator() {
   create_loading_screen()
-  load_module('Mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.js')
+  load_module('Mousetrap', 'https://craig.global.ssl.fastly.net/js/mousetrap/mousetrap.js?a4098')
   // TODO: setInterval?
 }
 
