7e9ba846c9dbc5b8105588fd30a3323f2fece793
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 14:59:33 -0500

    Give loading dots their own spans

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,11 @@
 import { create_element } from './create_element.js'
 
 function create_loading_screen() {
-  var loading_paragraph = create_element('p', 'Loading...')
+  var loading_dots = []
+    for (var dot = 1; dot <= 3; ++dot) {
+      loading_dots.push(create_element('span', '.', {'id': 'loadingdot' + dot.toString()}))
+    }
+  var loading_paragraph = create_element('p', ['Loading', ...loading_dots])
   var loading_container = create_element('div', loading_paragraph, {'id': 'scripterror'})
   
   var js_animator = document.getElementById('jsanimator')
