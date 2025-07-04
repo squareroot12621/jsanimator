2fc34bbaf0e11a321ba343c0c391d2b267541cd0
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 18:18:43 -0500

    Yes, of course it is.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { create_loading_screen, load_module } from './loading.js'
 
 function initialize_js_animator() {
   create_loading_screen()
-  load_module('Mousetrap', 'https://cdn.jsdelivr.net/gh/ccampbell/mousetrap/mousetrap.min.js')
+  load_modules()
   // TODO: setInterval?
 }
 
