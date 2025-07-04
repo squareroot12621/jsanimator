77098582b6ddfb63954cceb70eb6a7f1c55ade95
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 14:35:51 -0500

    Fix bug where content=[] errored

--- js/create_element.js
+++ js/create_element.js
@@ -1,8 +1,10 @@
 function create_element(tag, content=[], attributes={}) {
   var element = document.createElement(tag)
-  /* Passing a string should still work,
-     because it's an array of characters */
-  element.append(...(content ?? []))
+  if (content) {
+    /* Passing a string should still work,
+       because it's an array of characters */
+    element.append(...content)
+  }
   for (var [key, value] of Object.entries(attributes)) {
     element.setAttribute(key, value)
   }
