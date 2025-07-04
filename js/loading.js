b2ed838484ed74d932284e64927aa1ac7763f489
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 14:32:57 -0500

    Use more create_element

--- js/loading.js
+++ js/loading.js
@@ -2,10 +2,7 @@ import { create_element } from './create_element.js'
 
 function create_loading_screen() {
   var loading_paragraph = create_element('p', 'Loading...')
-  
-  var loading_container = document.createElement('div')
-  loading_container.setAttribute('id', 'scripterror')
-  loading_container.append(loading_paragraph)
+  var loading_container = create_element('div', loading_paragraph, {'id': 'scripterror'})
   
   var js_animator = document.getElementById('jsanimator')
   js_animator.replaceChildren(loading_container)
