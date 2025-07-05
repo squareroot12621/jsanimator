0fe97b841cdc9485691a4a33a53588bbccbf8162
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 19:05:05 -0500

    Forgot all the "var"s

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ function initialize_js_animator() {
 }
 
 function create_error_screen(js_error_text) {
-  error_heading = create_element('h2', 'Uh oh!')
+  var error_heading = create_element('h2', 'Uh oh!')
   
-  error_description_text = 'An error occurred in JS Animator. '
-                           + 'To fix the issue, try refreshing your browser. '
-                           + "If the error persists, don't be afraid to "
-  error_description_link_text = 'create an issue in the GitHub repository.'
-  error_description_link_reference = 'https://github.com/squareroot12621/jsanimator/issues/new/choose'
-  error_description_link = create_element('a', error_description_link_text,
-                                          {'href': error_description_link_reference})
-  error_description = create_element('p', [error_description_text, error_description_link_text])
+  var error_description_text = 'An error occurred in JS Animator. '
+                               + 'To fix the issue, try refreshing your browser. '
+                               + "If the error persists, don't be afraid to "
+  var error_description_link_text = 'create an issue in the GitHub repository.'
+  var error_description_link_reference = 'https://github.com/squareroot12621/jsanimator/issues/new/choose'
+  var error_description_link = create_element('a', error_description_link_text,
+                                              {'href': error_description_link_reference})
+  var error_description = create_element('p', [error_description_text, error_description_link_text])
 
-  error_nerd_text = "If you're a nerd (like me), this error text will be useful:"
-  error_nerd = create_element('p', error_nerd_text)
+  var error_nerd_text = "If you're a nerd (like me), this error text will be useful:"
+  var error_nerd = create_element('p', error_nerd_text)
 
-  error_box = error_create_element('p', js_error_text)
+  var error_box = error_create_element('p', js_error_text)
 
-  js_animator = document.getElementById('jsanimator')
+  var js_animator = document.getElementById('jsanimator')
   js_animator.replaceChildren(error_heading, error_description, error_nerd, error_box)
 }
 
