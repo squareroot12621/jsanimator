7b1a02e4c4b756e6bcac22b3c113eab1131221a6
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 09:24:24 -0500

    Further rephrasing (because all bugs are notable)

--- js/main.js
+++ js/main.js
@@ -34,25 +34,29 @@ function create_error_screen(error) {
 
   // Create the HTML
   var heading = create_element('h2', 'Uh oh!')
-  var description_text = 'An error occurred in JS Animator. '
-                         + 'To fix the issue, try refreshing your browser. '
-                         + "If the error persists, don't be afraid to "
+  var description_text_before = 'A critical error occurred in JS Animator. '
+                                + 'To report this bug, '
   var description_link_text = 'open an issue in the GitHub repository'
+  var description_text_after = ". Don't forget to include the procedure "
+                               + 'you took to get this error!'
   var description_link_reference = (
     'https://github.com/squareroot12621/jsanimator/issues/new/choose')
-  var description_link = create_element('a', description_link_text,
-                                        {'href': description_link_reference})
-  var description = create_element('p',
-                                   [description_text, description_link, '.'])
+  var description_link = create_element(
+    'a', description_link_text, {'href': description_link_reference}
+  )
+  var description = create_element(
+    'p', [description_text_before, description_link, description_text_after]
+  )
   var info_description_text = 'If you do open a GitHub issue about this, '
                               + 'this error text will be useful:'
   var info_description = create_element('p', info_description_text)
-  var error_info = create_element('pre', get_error_info(error),
-                                  {'id': 'errorinfo'})
-  var noscript_container = create_element('div',
-                                          [heading, description,
-                                           info_description, error_info],
-                                          {'id': 'scripterror'})
+  var error_info = create_element(
+    'pre', get_error_info(error), {'id': 'errorinfo'}
+  )
+  var noscript_container = create_element(
+    'div', [heading, description, info_description, error_info],
+    {'id': 'scripterror'}
+  )
   var js_animator = document.getElementById('jsanimator')
   js_animator.replaceChildren()
   js_animator.append(noscript_container)
