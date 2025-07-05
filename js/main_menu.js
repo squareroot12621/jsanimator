30e8468c2bd82d0321e9e8a5ae35f27a0af3b0be
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 14:54:47 -0500

    Add the buttons. They don't do anything yet though.

--- js/main_menu.js
+++ js/main_menu.js
@@ -2,6 +2,15 @@ import { create_element, update_root } from './utilities.js'
 
 function create_main_menu() {
   var title = create_element('h1', 'JS Animator')
+
+  var new_button = create_element(
+    'button', 'New', {class: 'primarybutton'}
+  )
+  var open_button = create_element('button', 'Open')
+  var settings_button = create_element('button', 'Settings')
+  var button_group = create_element(
+    'div', [new_button, open_button, settings_button]
+  )
   
   update_root(title)
 }
