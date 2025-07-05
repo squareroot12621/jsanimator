920a7ec63c2d3b7c54665b5c734a414d797bd2a9
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 10:30:22 -0500

    Replace tabs with spaces

--- js/utilities.js
+++ js/utilities.js
@@ -1,12 +1,12 @@
 function once(fn, context) { 
-	var result;
-	return function() { 
-		if(fn) {
-			result = fn.apply(context || this, arguments);
-			fn = context = null;
-		}
-		return result;
-	};
+  var result;
+  return function() { 
+    if (fn) {
+      result = fn.apply(context || this, arguments);
+      fn = context = null;
+    }
+    return result;
+  };
 }
-
+// dummy comment
 export { once }
