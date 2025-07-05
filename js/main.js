e076a0cc4805a83ce6b78bc8bf0105bc285f9845
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-04 21:57:34 -0500

    Or were they...?

--- js/main.js
+++ js/main.js
@@ -31,6 +31,9 @@ function create_error_screen(error) {
   
   var js_animator = document.getElementById('jsanimator')
   js_animator.replaceChildren(noscript_container)
+
+  // Exit immediately
+  throw Error()
 }
 
 window.addEventListener('error', create_error_screen)
