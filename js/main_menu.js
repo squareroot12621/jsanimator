02b225ba6e75ccab68448537e8d0d89121078c39
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 18:07:14 -0500

    Give the buttons descriptions

--- js/main_menu.js
+++ js/main_menu.js
@@ -3,18 +3,33 @@ import {create_element, update_root} from './utilities.js'
 function create_main_menu() {
   var title = create_element('h1', 'JS Animator')
 
-  var new_button = create_element(
-    'button', 'New', {class: 'largebutton primarybutton'}
-  )
-  var open_button = create_element(
-    'button', 'Open', {class: 'largebutton'}
-  )
-  var settings_button = create_element(
-    'button', 'Settings', {class: 'largebutton'}
-  )
-  var button_group = create_element(
-    'div', [new_button, open_button, settings_button]
-  )
+  var button_group = create_element('div', [], {id: 'menubuttongroup'})
+  
+  var button_parameters = [
+    {name: 'New',
+     class: 'primarybutton',
+     description: 'Start a new animation'},
+    {name: 'Open',
+     class: '',
+     description: 'Continue a previous project'},
+    {name: 'Settings',
+     class: '',
+     description: 'Customize your workspace'},
+  ]
+  for (var button in button_parameters) {
+    var {button_name, button_extra_classes, button_description} = button
+    button_classes = (
+      `fixedwidthbutton largebutton ${button_extra_classes}`.trim()
+    )
+    new_button = create_element(
+      'button', button_name, {class: button_classes}
+    )
+    new_description = create_element(
+      'div', button_description, {class: 'buttondescription'}
+    )
+    new_button_wrapper = create_element('div', [new_button, new_description])
+    button_group.append(new_button_wrapper)
+  }
   
   update_root(title, button_group)
 }
