4ac49b87916b27fef1c869168dc75df22fe83b89
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 15:06:16 -0500

    Make the buttons larger

--- js/main_menu.js
+++ js/main_menu.js
@@ -4,10 +4,14 @@ function create_main_menu() {
   var title = create_element('h1', 'JS Animator')
 
   var new_button = create_element(
-    'button', 'New', {class: 'primarybutton'}
+    'button', 'New', {class: 'largebutton primarybutton'}
   )
-  var open_button = create_element('button', 'Open')
-  var settings_button = create_element('button', 'Settings')
+  var open_button = create_element(
+    'button', 'Open', {class: 'largebutton'}
+  )
+  var settings_button = create_element(
+    'button', 'Settings', {class: 'largebutton'}
+  )
   var button_group = create_element(
     'div', [new_button, open_button, settings_button]
   )
