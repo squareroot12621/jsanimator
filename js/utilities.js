9fc01ef13e3a15008bcf953be48db6f82d0fc825
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 10:27:43 -0500

    Change because it wasn't working

--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,12 @@
-function once(func) {
-  var executed = false;
-  return function() {
-    if (!executed) {
-      return func(arguments)
-      executed = true;
-    }
-  }
+function once(fn, context) { 
+	var result;
+	return function() { 
+		if(fn) {
+			result = fn.apply(context || this, arguments);
+			fn = context = null;
+		}
+		return result;
+	};
 }
 
 export { once }
