8b2b1173cf256c0d8a952802c1c7ce8ce3b49719
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-05 21:28:01 -0500

    Actually the customization thing didn't work because Open needs to be <input type="file">

--- js/main_menu.js
+++ js/main_menu.js
@@ -3,34 +3,49 @@ import {create_element, update_root} from './utilities.js'
 function create_main_menu() {
   var title = create_element('h1', 'JS Animator')
 
-  var button_group = create_element('div', [], {id: 'menubuttongroup'})
-  
-  var button_parameters = [
-    {name: 'New',
-     class: 'primarybutton',
-     description: 'Start a new animation'},
-    {name: 'Open',
-     class: '',
-     description: 'Continue a previous project'},
-    {name: 'Settings',
-     class: '',
-     description: 'Customize your workspace'},
-  ]
-  for (var button of button_parameters) {
-    var all_classes = (
-      `narrowbutton largebutton ${button.class}`.trim()
-    )
-    var new_button = create_element(
-      'button', button.name, {class: all_classes}
-    )
-    var new_description = create_element(
-      'div', button.description, {class: 'buttondescription'}
-    )
-    var new_button_wrapper = create_element(
-      'div', [new_button, new_description], {class: 'menubuttonwrapper'}
-    )
-    button_group.append(new_button_wrapper)
-  }
+  var new_button = create_element(
+    'button', 'New', {class: 'narrowbutton largebutton primarybutton'}
+  )
+  var new_description = create_element(
+    'div', 'Start a new animation', {class: 'buttondescription'}
+  )
+  var new_wrapper = create_element(
+    'div', [new_button, new_description], {class: 'menubuttonwrapper'}
+  )
+
+  var open_button = create_element(
+    'input',
+    'Open',
+    {type: 'file',
+     id: 'openbutton',
+     name: 'open-file',
+     class: 'button narrowbutton largebutton',
+    }
+  )
+  var open_description = create_element(
+    'div', 'Continue a previous project', {class: 'buttondescription'}
+  )
+  var open_wrapper = create_element(
+    'div', [open_button, open_description], {class: 'menubuttonwrapper'}
+  )
+
+  var settings_button = create_element(
+    'button', 'Settings', {class: 'narrowbutton largebutton'}
+  )
+  var settings_description = create_element(
+    'div', 'Customize your workspace', {class: 'buttondescription'}
+  )
+  var settings_wrapper = create_element(
+    'div',
+    [settings_button, settings_description],
+    {class: 'menubuttonwrapper'}
+  )
+
+  var button_group = create_element(
+    'div',
+    [new_wrapper, open_wrapper, settings_wrapper],
+    {id: 'menubuttongroup'}
+  )
   
   update_root(title, button_group)
 }
