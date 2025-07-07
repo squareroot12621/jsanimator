c847f266d87a59a0ea42748c2bbb6468bda59ae6
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-06 20:28:49 -0500

    Use globals.modules instead of modules

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 import {create_loading_screen, load_modules} from './loading.js'
 import {create_main_menu} from './main_menu.js'
 
-import {modules} from './globals.js'
+import {globals} from './globals.js'
 import {once} from './utilities.js'
 import {create_error_screen} from './error.js'
 
 async function initialize_js_animator() {
   create_loading_screen()
-  modules = await load_modules()
+  globals.modules = await load_modules()
   create_main_menu()
 }
 
