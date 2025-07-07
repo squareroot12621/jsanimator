ec0809c856a8c0db149dad532944d7aebcd92f1b
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-07 11:02:32 -0500

    Add test Object.keys()

--- js/editing.js
+++ js/editing.js
@@ -2,7 +2,7 @@ import {globals} from './globals.js'
 import {create_element, update_root} from './utilities.js'
 
 function create_editing_screen() {
-  console.log(globals.currentFile)
+  console.log(Object.keys(globals.currentFile))
   var title = create_element('h1', globals.currentFile['Test1 - Copy/svgs/rectangle.svg'].slice(0, 50))
 
   update_root(title)
