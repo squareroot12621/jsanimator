fbaaaa11d1c06f58e04c63b51a083efd68cf022e
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-07 08:39:20 -0500

    Adhere to the 79-character limit

--- js/loading.js
+++ js/loading.js
@@ -3,24 +3,25 @@ import {create_element, update_root} from './utilities.js'
 function create_loading_screen() {
   var loading_dots = []
     for (var dot = 1; dot <= 3; ++dot) {
-      loading_dots.push(create_element('span',
-                                       '.',
-                                       {'class': 'loadingdot',
-                                        'id': `loadingdot${dot.toString()}`}))
+      loading_dots.push(create_element(
+        'span', '.',
+        {'class': 'loadingdot', 'id': `loadingdot${dot.toString()}`}
+      ))
     }
-  var loading_paragraph = create_element('p',
-                                         ['Loading', ...loading_dots],
-                                         {'id': 'loadingtext'})
-  var loading_progress = create_element('output',
-                                        '0%',
-                                        {'id': 'loadingprogress'})
+  var loading_paragraph = create_element(
+    'p', ['Loading', ...loading_dots], {'id': 'loadingtext'}
+  )
+  var loading_progress = create_element(
+    'output', '0%', {'id': 'loadingprogress'}
+  )
   loading_progress.style.setProperty('font-weight', 200)
-  var loading_info = create_element('output',
-                                    'Loading third-party modules...',
-                                    {'id': 'loadinginfo'})
-  var loading_container = create_element('div',
-                                         [loading_paragraph, loading_progress, loading_info],
-                                         {'id': 'loadingcontainer'})
+  var loading_info = create_element(
+    'output', 'Loading third-party modules...', {'id': 'loadinginfo'}
+  )
+  var loading_container = create_element(
+    'div', [loading_paragraph, loading_progress, loading_info],
+    {'id': 'loadingcontainer'}
+  )
   
   update_root(loading_container)
 }
