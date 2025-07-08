c7994f1fb461f0ad9c584c243e202ec49964a920
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-08 07:39:42 -0500

    No.

--- js/main_menu.js
+++ js/main_menu.js
@@ -62,10 +62,9 @@ function create_main_menu() {
 
   open_input.onchange = async function () {
     open_input.onchange = function () {}
-    unzip(open_input.files[0]).then(function () {
-      console.log(`current_file #2: ${JSON.stringify(globals.current_file)}`)
-      create_editing_screen()
-    })
+    await unzip(open_input.files[0])
+    console.log(`current_file #2: ${JSON.stringify(globals.current_file)}`)
+    create_editing_screen()
   }
 }
 
