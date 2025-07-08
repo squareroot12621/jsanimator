882bd3962ca8bcffc92388865d8145921543f182
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-08 11:35:00 -0500

    Rereadd 2.9

--- js/main_menu.js
+++ js/main_menu.js
@@ -85,6 +85,7 @@ async function unzip(file) {
   console.log(`-- ORDERING TEST 2\nzip.files: ${JSON.stringify(zip.files).slice(0, 100)}`)
   return Promise.all(promises())
     .then((directory) => {
+      console.log(`-- ORDERING TEST 2.9\ndirectory: ${JSON.stringify(directory).slice(0, 100)}`)
       globals.current_file = Object.fromEntries(directory)
       console.log(`-- ORDERING TEST 3\ncurrent_file: ${JSON.stringify(globals.current_file).slice(0, 100)}`)
       return null
