7051965d9d5f6eade02481a2eaac7e14fe8ea979
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-08 11:00:30 -0500

    Split up the two asyncs

--- js/main_menu.js
+++ js/main_menu.js
@@ -68,23 +68,21 @@ function create_main_menu() {
   }
 }
 
-function unzip(file) {
-  return new Promise((resolve, reject) => {
+async function unzip(file) {
+  var zip = await new Promise((resolve, reject) => {
     console.log('-- ORDERING TEST 1')
     resolve(JSZip.loadAsync(file))
-  }).then(async (zip) => {
+    console.log('-- ORDERING TEST 1.5')
+  })
+  return new Promise((resolve, reject) => {
     console.log(`-- ORDERING TEST 2\nzip.files: ${JSON.stringify(zip.files).slice(0, 100)}`)
-    var directory = await new Promise((resolve, reject) => {
-      resolve(Object.fromEntries(
-        Object.entries(zip.files).map(async function ([key, val]) {
-          var unzipped = await val.async('string')
-          console.log(`-- ORDERING TEST 2.5\nunzipped: ${unzipped.slice(0, 100)}`)
-          return [key, unzipped]
-        })
-      ))
-    })
-    console.log(`-- ORDERING TEST 2.9\ndirectory: ${JSON.stringify(directory).slice(0, 100)}`)
-    return directory
+    resolve(Object.fromEntries(
+      Object.entries(zip.files).map(async function ([key, val]) {
+        var unzipped = await val.async('string')
+        console.log(`-- ORDERING TEST 2.5\nunzipped: ${unzipped.slice(0, 100)}`)
+        return [key, unzipped]
+      })
+    ))
   }).then((directory) => {
     globals.current_file = directory
     console.log(`-- ORDERING TEST 3\ncurrent_file: ${JSON.stringify(globals.current_file).slice(0, 100)}`)
