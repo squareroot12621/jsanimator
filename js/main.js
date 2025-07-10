123349a427ef7a486aed37f722906fed84d29e1c
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-10 12:32:20 -0500

    Add resize_root

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import {create_loading_screen, load_modules} from './loading.js'
 import {create_main_menu} from './main_menu.js'
 
 import {globals} from './globals.js'
-import {once} from './utilities.js'
+import {resize_root, once} from './utilities.js'
 import {create_error_screen} from './error.js'
 
 async function initialize_js_animator() {
+  resize_root()
   create_loading_screen()
   globals.modules = await load_modules()
   create_main_menu()
@@ -14,4 +15,5 @@ async function initialize_js_animator() {
 var create_error_screen_wrapper = once(create_error_screen)
 window.addEventListener('error', create_error_screen_wrapper)
 window.addEventListener('unhandledrejection', create_error_screen_wrapper)
+window.addEventListener('resize', resize_root)
 window.onload = initialize_js_animator()
