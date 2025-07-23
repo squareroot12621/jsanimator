ddb5c670e01373c354053832f214beace1a01605
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-23 14:43:00 -0500

    Unexpected token '...'

--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,10 @@
 function create_element(tag, content=[], attributes={}) {
   var element = document.createElement(tag)
-  element.append(content instanceof Array ? ...content : content)
+  if (content instanceof Array) {
+    element.append(...content)
+  } else {
+    element.append(content)
+  }
   for (var [key, value] of Object.entries(attributes)) {
     element.setAttribute(key, value)
   }
