4f0f3f8797e1b00dd427ef780e6200a987ac3896
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-26 09:33:55 -0500

    Fix window.onload and make it more consistent

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import {create_loading_screen, load_modules} from './loading.js'
 import {create_main_menu} from './main_menu.js'
 
 import {globals} from './globals.js'
-import {resize_root, once} from './utilities.js'
+import {resize_root, resize_root_instant, once} from './utilities.js'
 import {create_error_screen} from './error.js'
 
 async function initialize_js_animator() {
-  resize_root()
+  resize_root_instant()
   create_loading_screen()
   globals.modules = await load_modules()
   create_main_menu()
@@ -16,4 +16,4 @@ var create_error_screen_wrapper = once(create_error_screen)
 window.addEventListener('error', create_error_screen_wrapper)
 window.addEventListener('unhandledrejection', create_error_screen_wrapper)
 window.addEventListener('resize', resize_root)
-window.onload = initialize_js_animator()
+window.addEventListener('load', initialize_js_animator)
