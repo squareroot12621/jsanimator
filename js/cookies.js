f6204bd2df6fdc46e13fd1a8fbee7c9a25c2d775
squareroot12621 <mathandcoding2718@gmail.com>
2025-07-27 12:15:16 -0500

    Do more work on cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -4,30 +4,57 @@ import {globals} from './globals.js'
 async function create_cookie_screen() {
   globals.screen = 'cookies'
 
-  var heading = create_element('h2', 'This site uses cookies')
+  var cookies_available = local_storage_available()
 
-  var body_text = 'JS Animator uses cookies to store '
-                  + 'preferences locally on your device, '
-                  + "meaning they won't be sent to servers "
-                  + 'or advertising companies. '
-                  + 'You can block these cookies, '
-                  + 'but this will stop preferences '
-                  + 'from being saved after leaving the app. '
-                  + 'Your decision is stored '
-                  + "so you don't have to do it again, "
-                  + 'and you can change your decision '
-                  + 'at any time.'
+  var heading = create_element(
+    'h2',
+    cookies_available ? 'This site uses cookies' : 'Cookies are disabled'
+  )
+  if (cookies_available) {
+    var body_text = 'JS Animator uses cookies to store '
+                    + 'preferences locally on your device, '
+                    + "meaning they won't be sent to servers "
+                    + 'or advertising companies. '
+                    + 'You can block these cookies, '
+                    + 'but this will stop preferences '
+                    + 'from being saved after leaving the app. '
+                    + 'Your decision is stored '
+                    + "so you don't have to do it again, "
+                    + 'and you can change your decision '
+                    + 'at any time.'
+  } else {
+    var body_text = 'Cookies are disabled in your browser, '
+                    + 'so preferences you set in JS Animator '
+                    + 'will not save if you leave. '
+                    + 'You can reenable cookies in your browser '
+                    + 'to keep your preferences, '
+                    + "but you don't have to."
+  }
   var body = create_element('p', body_text)
 
-  var block_button = create_element(
-    'button', 'Block', {class: 'narrowbutton largebutton warningbutton'}
-  )
-  var allow_button = create_element(
-    'button', 'Allow', {class: 'narrowbutton largebutton primarybutton'}
-  )
+  if (cookies_available) {
+    var buttons = [
+      create_element(
+        'button', 'Block', {class: 'narrowbutton largebutton warningbutton'}
+      ),
+      create_element(
+        'button', 'Allow', {class: 'narrowbutton largebutton primarybutton'}
+      ),
+    ]
+  } else {
+    var buttons = [
+      create_element(
+        'button', 'OK', {class: 'narrowbutton largebutton primarybutton'}
+      ),
+    ]
+  }
 
+  var button_container = create_element(
+    'div', buttons, {class: ''}
+  )
+  
   var cookie_container = create_element(
-    'div', [heading, body, block_button, allow_button],
+    'div', [heading, body, button_container],
     /* Technically this isn't a script error,
        but it needs to be styled like one */
     {'id': 'scripterror'}
@@ -35,12 +62,11 @@ async function create_cookie_screen() {
   update_root(cookie_container)
 
   var result = await new Promise((resolve, reject) => {
-    block_button.addEventListener('click', () => {
-      resolve('block')
-    })
-    allow_button.addEventListener('click', () => {
-      resolve('allow')
-    })
+    for (let button of buttons) {
+      button.addEventListener('click', () => {
+        resolve(button.innerText)
+      })
+    }
   })
   console.log(result)
   return result
