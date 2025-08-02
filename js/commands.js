97037ab20b38466a0ed2d7e460058eb171860641
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-02 17:04:04 -0500

    Move save logic to another function

--- js/commands.js
+++ js/commands.js
@@ -1,5 +1,23 @@
 import {globals} from './globals.js'
 
+function save(name=null) {
+  /* Put all the files back into a JSZip().
+     JSZip automatically handles making files inside of folders. */
+  var zip = new JSZip()
+  for (var [filename, contents] of Object.entries(globals.current_file)) {
+    // Remove the topmost directory because it's the name of the file
+    var new_filename = filename.replace(/.*?\//, '')
+    zip.file(new_filename, contents)
+  }
+  // Then save the .zip file!
+  zip.generateAsync({
+    type: 'blob',
+    compression: 'DEFLATE',
+  }).then(function(content) {
+    saveAs(content, name)
+  })
+}
+
 function handle_action(command_name) {
   if (globals.screen === 'main') {
     if (command_name === 'new_file') {
@@ -18,18 +36,7 @@ function handle_action(command_name) {
     }
   } else if (globals.screen === 'editing') {
     if (command_name === 'save_file') {
-      /* Put all the files back into a JSZip().
-         JSZip automatically handles making files inside of folders. */
-      var zip = new JSZip()
-      for (var [filename, contents] of Object.entries(globals.current_file)) {
-        // Remove the topmost directory because it's the name of the file
-        var new_filename = filename.replace(/.*?\//, '')
-        zip.file(new_filename, contents)
-      }
-      // Then save the .zip file!
-      zip.generateAsync({type: 'blob'}).then(function(content) {
-        saveAs(content, 'Output Test.anj')
-      })
+      save('Output Test.anj')
     } else if (command_name === 'save_as') {
       console.log(globals.current_file) // TODO: Do something else
     } else {
