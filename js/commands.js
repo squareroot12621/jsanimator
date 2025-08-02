278e1f0fbc988a6dfcf686650d0cd6e73786ed86
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-02 08:28:09 -0500

    Add globals.screen to the console.log and the comment to the function

--- js/commands.js
+++ js/commands.js
@@ -1,6 +1,22 @@
+import {globals} from './globals.js'
+
 function handle_action(command_name) {
-  console.log(command_name) // TODO: Do something else
+  console.log(globals.screen, command_name) // TODO: Do something else
   return false
+
+  /* Export zip file with name
+  
+  zip.generateAsync({
+    type: "base64"
+  }).then(function(content) {
+    var link = document.createElement('a');
+    link.href = "data:application/zip;base64," + content;
+    link.download = "your-file-name.zip";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+  */
 }
 
 export {handle_action}
